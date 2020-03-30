27f9418317f8160e10bf8a5ab88df3dac0da81af
MarvRoldan <marvinroldan_@hotmail.com>
2020-03-30 10:31:27 -0600

    Deleted solution. They were easy

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,3 @@
 function redundant(str) {
 	return () => str
 }
-
-// -- Return the Remainder from Two Numbers --
-var remainder = (x, y) => x % y;
-
-
-// -- Maximum Edge of a Triangle --
-var nextEdge = (side1, side2) => (side1 + side2) - 1;
-
-// -- Less Than 100? --
-function lessThan100(a, b) {
-	if ((a + b) < 100) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-// -- Convert Minutes into Seconds --
-var convert = (minutes) => minutes * 60;
-
-// -- Is the Number Less than or Equal to Zero? --
