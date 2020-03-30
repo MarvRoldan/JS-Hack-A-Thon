336f745401222873845b348586a9699461e24fd8
MarvRoldan <marvinroldan_@hotmail.com>
2020-03-30 13:07:19 -0600

    Added solution 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
 // -- A Redundant Function --
-var redundant = (str) => () => str;
+var redundant = ( str ) => () => str;
 
 // -- Remove Trailing and Leading Zeros --
-var removeLeadingTrailing = (n) => parseFloat(n)+'';
+var removeLeadingTrailing = ( n ) => parseFloat( n ) + "";
 
 // -- Array of Multiples --
-var arrayOfMultiples = (num, length) => {
+var arrayOfMultiples = ( num, length ) => {
 	var array = []
 	for (i = 1; i <= length; i++) {
 		array.push( num * i );
 	}
 	return array;
+}
+
+// -- Convert Key, Values in an Object to Array --
+var objectToArray = ( obj ) => Object.entries( obj );
+
+
+// -- Make a Circle with OOP -- 
+class Circle {
+    constructor( radius ) {
+    this.radius = radius;
+    }
+      
+    getArea(){ return this.radius * this.radius * Math.PI };
+    getPerimeter(){ return this.radius * Math.PI * 2 };
 }
