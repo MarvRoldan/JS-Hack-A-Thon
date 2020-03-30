f94d42688b352b35b09f02adb4ec19268bb232fa
MarvRoldan <marvinroldan_@hotmail.com>
2020-03-30 11:14:55 -0600

    Added solution 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
 // -- A Redundant Function --
-function redundant(str) {
-	return () => str
+var redundant = (str) => () => str;
+
+// -- Remove Trailing and Leading Zeros --
+var removeLeadingTrailing = (n) => parseFloat(n)+'';
+
+// -- Array of Multiples --
+var arrayOfMultiples = (num, length) => {
+	var array = []
+	for (i = 1; i <= length; i++) {
+		array.push( num * i );
+	}
+	return array;
 }
