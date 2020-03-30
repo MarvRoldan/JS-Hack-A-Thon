0f234eb78799e4065cfe68818314a1e594d95746
MarvRoldan <marvinroldan_@hotmail.com>
2020-03-30 10:12:58 -0600

    Changing solutions to ES6

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,11 @@ function redundant(str) {
 	return () => str
 }
 
+// -- Return the Remainder from Two Numbers --
+var remainder = (x, y) => x % y;
+
+
+// -- Maximum Edge of a Triangle --
+var nextEdge = (side1, side2) => (side1 + side2) - 1;
+
+
